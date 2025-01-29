ee004b0240364b1c2e8d1ae3a4cb6c9bb1ccc38f
Shane Reall <Shaneareall@gmail.com>
2025-01-28 22:07:44 -0500

    Ability Commit Final
    Finished commenting in all abilities that affect the damage calculations. Next plan is to begin programming in the actual abilities.

--- demo/Battle Calculator/src/abilityList.java
+++ demo/Battle Calculator/src/abilityList.java
@@ -122,120 +122,120 @@ public enum abilityList { //Will Become HashMap but is an Enum for Now
     Klutz,              //Item Buffs and Debuffs = 0
     Leaf_Guard,
     Levitate,
-    Libero,             //Also has STAB
+    Libero,             //All has STAB
     Light_Metal,        //Halves Weight Rounded Down
     Lightning_Rod,      //Electric Immunity
     Limber,
     Lingering_Aroma,
     Liquid_Ooze,
-    Liquid_Voice,
-    Long_Reach,
+    Liquid_Voice,       //Set Type Water in List Sound Moves
+    Long_Reach,         //Removes Contact From Moves
     Magic_Bounce,
     Magic_Guard,
     Magician,
     Magma_Armor,
     Magnet_Pull,
-    Marvel_Scale,
-    Mega_Launcher,
-    Merciless,
+    Marvel_Scale,       //Statused = Defense +50%
+    Mega_Launcher,      //+50% Damage to List Aura and Pulse Moves
+    Merciless,          //Target Poisoned = 100% Crit
     Mimicry,
-    Minds_Eye,
-    Minus,
+    Minds_Eye,          //Negate Ghost Immunity to Normal & Fighting
+    Minus,              //Ally has Plus = +50% Special Attack
     Mirror_Armor,
     Misty_Surge,
-    Mold_Breaker,
+    Mold_Breaker,       //Ignore all Opponent Abilities in List Ignorable
     Moody,
-    Motor_Drive,
+    Motor_Drive,        //Immunity Electric
     Moxie,
-    Multiscale,
+    Multiscale,         //HP = 100% -> Opponent Damage -50%
     Multitype,
     Mummy,
-    Mycelium_Might,
+    Mycelium_Might,     //Ignore all Opponent Abilities in List Ignorable
     Natural_Cure,
-    Neuroforce,
-    Neutralizing_Gas,
+    Neuroforce,         //Super Effective Attack = +25% Damage
+    Neutralizing_Gas,   //Ignore all Abilities in List Ignorable+
     No_Guard,
-    Normalize,
+    Normalize,          //All Attacks Become Normal and Get a 20% Power Boost, Except Exceptions
     Oblivious,
     Opportunist,
-    Orichalcum_Pulse,
+    Orichalcum_Pulse,   //Sun = Attack +33%
     Overcoat,
-    Overgrow,
+    Overgrow,           //33% HP = +50% Grass Moves
     Own_Tempo,
-    Parental_Bond,
+    Parental_Bond,      //Hard Coded into Damage Calculation for some Reason
     Pastel_Veil,
     Perish_Body,
     Pickpocket,
     Pickup,
-    Pixilate,
-    Plus,
+    Pixilate,           //+20% Damage on Normal Moves & Normal Moves Become Flying
+    Plus,               //Ally has Minus = +50% Special Attack
     Poison_Heal,
     Poison_Point,
     Poison_Puppeteer,
     Poison_Touch,
     Power_Construct,
     Power_of_Alchemy,
-    Power_Spot,
+    Power_Spot,         //Allies Power +30%
     Prankster,
     Pressure,
     Primordial_Sea,
-    Prism_Armor,
+    Prism_Armor,        //Super Effective Hit = Damage -25%
     Propeller_Tail,
-    Protean,
-    Protosynthesis,
+    Protean,            //All has STAB
+    Protosynthesis,     //Boost = +50% Speed ELSE +30% Highest
     Psychic_Surge,
-    Punk_Rock,
-    Pure_Power,
-    Purifying_Salt,
-    Quark_Drive,
-    Queenly_Majesty,
+    Punk_Rock,          //+30% Power Sound Moves, -50% Damage Sound Moves
+    Pure_Power,         //Attack *2
+    Purifying_Salt,     //-50% Offensive Stat From Ghost Moves
+    Quark_Drive,        //Boost = +50% Speed ELSE +30% Highest
+    Queenly_Majesty,    //Immune to Moves List Priority
     Quick_Draw,
-    Quick_Feet,
+    Quick_Feet,         //Statused = Speed +50%
     Rain_Dish,
     Rattled,
     Receiver,
-    Reckless,
-    Refrigerate,
+    Reckless,           //Move Recoils/Crashes = +20% Power
+    Refrigerate,        //+20% Damage on Normal Moves & Normal Moves Become Ice
     Regenerator,
     Ripen,
-    Rivalry,
+    Rivalry,            //Same Gender = Power +25% || Opposite Gender = Power -25%
     RKS_System,
-    Rock_Head,
-    Rocky_Payload,
+    Rock_Head,          //Disable Recoil Damage
+    Rocky_Payload,      //Add Rock STAB
     Rough_Skin,
     Run_Away,
-    Sand_Force,
-    Sand_Rush,
+    Sand_Force,         //Weather = Sandstorm -> +30% Power Rock, Ground, and Steel
+    Sand_Rush,          //Weather = Sandstorm -> *2 Speed
     Sand_Spit,
     Sand_Stream,
     Sand_Veil,
-    Sap_Sipper,
+    Sap_Sipper,         //Immunity Grass
     Schooling,
-    Scrappy,
+    Scrappy,            //Negate Ghost Immunity to Normal & Fighting
     Screen_Cleaner,
     Seed_Sower,
     Serene_Grace,
-    Shadow_Shield,
+    Shadow_Shield,      //HP = 100% -> Opponent Damage -50%
     Shadow_Tag,
-    Sharpness,
+    Sharpness,          //+50% Damage From List Slicing
     Shed_Skin,
-    Sheer_Force,
-    Shell_Armor,
+    Sheer_Force,        //Additional Effect Moves = +30% Power
+    Shell_Armor,        //Disable CRIT
     Shield_Dust,
     Shields_Down,
     Simple,
     Skill_Link,
-    Slow_Start,
-    Slush_Rush,
-    Sniper,
+    Slow_Start,         //50% Attack & Speed
+    Slush_Rush,         //Weather = Hail/Snow -> *2 Speed
+    Sniper,             //CRIT = Damage +50%
     Snow_Cloak,
     Snow_Warning,
-    Solar_Power,
-    Solid_Rock,
+    Solar_Power,        //Weather = Sun -> *1.5 Special Attack
+    Solid_Rock,         //Super Effective -25% Damage
     SoulHeart,
-    Soundproof,
+    Soundproof,         //Immunity Sound List
     Speed_Boost,
-    Stakeout,
+    Stakeout,           //Opponent Switched In = Power *2
     Stall,
     Stalwart,
     Stamina,
@@ -243,66 +243,66 @@ public enum abilityList { //Will Become HashMap but is an Enum for Now
     Static,
     Steadfast,
     Steam_Engine,
-    Steelworker,
-    Steely_Spirit,
+    Steelworker,        //Add Steel STAB
+    Steely_Spirit,      //Add Steel STAB (Including Allies)
     Stench,
     Sticky_Hold,
-    Storm_Drain,
-    Strong_Jaw,
-    Sturdy,
+    Storm_Drain,        //Immunity Water
+    Strong_Jaw,         //Biting Moves = +50% Power
+    Sturdy,             //Immunity OHKO
     Suction_Cups,
     Super_Luck,
     Supersweet_Syrup,
-    Supreme_Overlord,
-    Surge_Surfer,
-    Swarm,
+    Supreme_Overlord,   //Power +10 For Fained Allies in Party
+    Surge_Surfer,       //Electric Terrain = *2 Speed
+    Swarm,              //33% HP = +50% Bug Moves
     Sweet_Veil,
-    Swift_Swim,
-    Sword_of_Ruin,
+    Swift_Swim,         // Weather = Rain -> *2 Speed
+    Sword_of_Ruin,      //Lower All Defense Stats by 25%
     Symbiosis,
     Synchronize,
-    Tablets_of_Ruin,
+    Tablets_of_Ruin,    //Lower All Attack Stats by 25%
     Tangled_Feet,
     Tangling_Hair,
-    Technician,
+    Technician,         //Power < 60 -> +50% Power
     Telepathy,
-    Tera_Shell,
+    Tera_Shell,         //HP 100% = All Damage Not very Effective
     Tera_Shift,
     Teraform_Zero,
-    Teravolt,
+    Teravolt,           //Ignore all Opponent Abilities in List Ignorable
     Thermal_Exchange,
-    Thick_Fat,
-    Tinted_Lens,
-    Torrent,
-    Tough_Claws,
-    Toxic_Boost,
+    Thick_Fat,          //Ice & Fire Attacks from Opponent = Offenses -50%
+    Tinted_Lens,        //Not Very Effective Attack = Damage *2
+    Torrent,            //33% HP = +50% Water Moves
+    Tough_Claws,        // Contact Attack = Power +30%
+    Toxic_Boost,        //Poisoned = Power +50% Physical
     Toxic_Chain,
     Toxic_Debris,
     Trace,
-    Transistor,
+    Transistor,         //Electric Move -> Offense Multiplied 1.5
     Triage,
     Truant,
-    Turboblaze,
-    Unaware,
-    Unburden,
+    Turboblaze,         //Ignore all Opponent Abilities in List Ignorable
+    Unaware,            //Ignore Stat Changes
+    Unburden,           //Speed Doubles
     Unnerve,
     Unseen_Fist,
-    Vessel_of_Ruin,
+    Vessel_of_Ruin,     //Lower All Defense Stats by 25%
     Victory_Star,
     Vital_Spirit,
-    Volt_Absorb,
+    Volt_Absorb,        //Immunity Electric
     Wandering_Spirit,
-    Water_Absorb,
-    Water_Bubble,
+    Water_Absorb,       //Immunity Water
+    Water_Bubble,       //Fire Damage = 1/2 && Water Attack = *2 Damage
     Water_Compaction,
     Water_Veil,
     Weak_Armor,
-    Well_Baked_Body,
+    Well_Baked_Body,    //Immunity Fire
     White_Smoke,
     Wimp_Out,
     Wind_Power,
     Wind_Rider,
-    Wonder_Guard,
+    Wonder_Guard,       //!Super Effective = No Damage
     Wonder_Skin,
     Zen_Mode,
     Zero_to_Hero
